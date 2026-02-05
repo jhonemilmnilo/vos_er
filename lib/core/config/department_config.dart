eb165dc0c743b06bab6a0044056db12343ba6f2b
jhonemilmnilo <nilojhonemil1@gmail.com>
2026-02-05 10:39:40 +0800

    some changes in the port

--- lib/core/config/department_config.dart
+++ lib/core/config/department_config.dart
@@ -32,19 +32,19 @@ const List<Department> availableDepartments = [
   Department(
     name: 'Hanvin',
     port: 8092,
-    baseUrl: 'http://192.168.0.143:8092',
+    baseUrl: 'http://100.110.197.61:8092',
     token: '', // No token required for Hanvin server
   ),
   Department(
     name: 'Human Resources',
     port: 8091,
-    baseUrl: 'http://192.168.0.143:8091',
+    baseUrl: 'http://100.110.197.61:8091',
     token: 'rTilKSsclzuQW8WfQWK1ba8wrD_LetNn', // Token required for HR server
   ),
   Department(
     name: 'Vertex',
     port: 8090,
-    baseUrl: 'http://192.168.0.143:8090',
+    baseUrl: 'http://100.110.197.61:8090',
     token: '', // No token required for Vertix server
   ),
 ];
